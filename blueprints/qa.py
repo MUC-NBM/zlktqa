4424a6fb9906801af12b3e34b528a753e536dee7
BLANMAN <2849115967@qq.com>
2023-11-28 20:51:10 +0800

    完整代码
    发布答案功能完成
    搜索功能的实现：

--- blueprints/qa.py
+++ blueprints/qa.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, url_for, request, render_template, g
-from .forms import QuestionForm
-from models import QuestionModel
+from .forms import QuestionForm, AnswerForm
+from models import QuestionModel, AnswerModel
 from exts  import db
 from decorators import  login_required
 bp = Blueprint("qa",__name__, url_prefix="/")#设置为首页
@@ -34,3 +34,29 @@ def public_question():
 def qa_detail(qa_id):
     question = QuestionModel.query.get(qa_id)
     return render_template("detail.html",question=question)
+
+
+# @bp.route("/answer/public",methods=['POST'])
+@bp.post("/answer/public")#新的写法
+@login_required
+def public_answer():
+    form = AnswerForm(request.form)
+    if form.validate():
+        content = form.content.data
+        question_id = form.question_id.data
+        answer = AnswerModel(content=content,question_id=question_id,author_id=g.user.id)
+        db.session.add(answer)
+        db.session.commit()
+        return redirect(url_for("qa.qa_detail",qa_id=question_id))
+    else:
+        print(form.errors)
+        return redirect(url_for("qa.qa_detail",qa_id=request.form.get("question_id")))
+
+@bp.route("/search")
+def search():
+# /search?q=flask
+# /search/<q>
+# post，request.form
+    q = request.args.get("q")
+    questions = QuestionModel.query.filter(QuestionModel.title.contains(q)).all()
+    return render_template("index.html",questions=questions)
